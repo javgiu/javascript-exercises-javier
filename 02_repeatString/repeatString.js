23dfa66da3e8f620c81e140d2edb17feb92a39d4
javgiu <javypicallo@gmail.com>
2025-02-06 22:22:08 -0500

    Resolve exrcises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,22 @@
-const repeatString = function() {
+const repeatString = function(text, times) {
 
+    if (times >= 0) {
+
+        let para = "";
+
+        for (let i = 0; i < times; i++) {
+            para += text;
+        }
+
+        return para;
+
+    } else {
+
+        return "ERROR";
+
+    }
+
+    
 };
 
 // Do not edit below this line
