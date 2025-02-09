93227793ff88098f3cf01d630296a0bfe63cffa8
javgiu <javypicallo@gmail.com>
2025-02-08 23:52:52 -0500

    Solve exercises 03 and 04, diferent from solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,26 @@
-const reverseString = function() {
+const reverseString = function(text) {
+
+    // Convert string to array
+    const textArray = text.split("");
+
+    const reverseArray = [];
+
+    for (let i = (textArray.length - 1); i >=0 ; i--) {
+
+        // Add array element from the last to the first
+        reverseArray.push(textArray[i]);
+
+    }
+
+    // Return array as a string without separators
+    return reverseArray.join("");
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
+
+/*
+    Check the solution for a short solution
+    
+    return tet.split("").reverse().join("");
+*/ 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (array, ...valueToRemove) {
+    for (const value of valueToRemove) {
+        for (let i = 0; i < array.length; i++) {
+            if (value === array[i]) {
+                array.splice(i, 1);
+                i--;
+            }
+            
+        }
+    }
+
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/* --- PLEASE CHECK THE SOLUTION */
